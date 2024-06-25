bded3a0333efd00999028f569c36346202bbdf62
u80861711 <diego.diaz@bit.admin.ch>
2024-06-25 14:39:07 +0200

    added generic print functions

--- name.go
+++ name.go
@@ -1,6 +1,9 @@
 package name
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 // Upper returns the uppercase of the given string argument.
 func Upper(s string) string {
@@ -10,4 +13,18 @@ func Upper(s string) string {
 // Lower returns the lowercase of the given string argument.
 func Lower(s string) string {
 	return strings.ToLower(s)
+}
+
+func printString(s string) { fmt.Println(s) }
+
+func printInt(i int) { fmt.Println(i) }
+
+func printBool(b bool) { fmt.Println(b) }
+
+func myStringIntBoolPrint[printThis string | int | bool](what printThis) {
+	fmt.Println(what)
+}
+
+func myGenericPrint[printThis any](what printThis) {
+	fmt.Println(what)
 }
--- name_test.go
+++ name_test.go
@@ -1,6 +1,8 @@
 package name
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestUpper(t *testing.T) {
 	type args struct {
@@ -22,13 +24,64 @@ func TestUpper(t *testing.T) {
 	}
 }
 
-
 func benchUpper(i string, b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		Upper(i)
 	}
 }
 
-func BenchmarkUpper1(b *testing.B) { benchUpper("a", b)}
-func BenchmarkUpper10(b *testing.B) { benchUpper("Hola que tal estas", b)}
-func BenchmarkUpper20(b *testing.B) { benchUpper("Este va a ser mas largo porque se necesita asi", b)}
+func BenchmarkUpper1(b *testing.B)  { benchUpper("a", b) }
+func BenchmarkUpper10(b *testing.B) { benchUpper("Hola que tal estas", b) }
+func BenchmarkUpper20(b *testing.B) { benchUpper("Este va a ser mas largo porque se necesita asi", b) }
+
+func TestPrintString(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			printString(tt.args.s)
+		})
+	}
+}
+
+func TestPrintInt(t *testing.T) {
+	type args struct {
+		i int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			printInt(tt.args.i)
+		})
+	}
+}
+
+func TestPrintBool(t *testing.T) {
+	type args struct {
+		b bool
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			printBool(tt.args.b)
+		})
+	}
+}
+
